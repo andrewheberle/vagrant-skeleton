8cf88d107033b39202426f06146560cb1ae91205
Andrew Heberle <andrew.heberle@gmail.com>
2015-12-18 14:30:15 +0800

    Added some comments

--- puppet/bootstrap/manifests/site.pp
+++ puppet/bootstrap/manifests/site.pp
@@ -1,8 +1,10 @@
+# Install r10k
 exec { 'r10k-install':
   command => '/opt/puppetlabs/puppet/bin/gem install r10k',
   creates => '/opt/puppetlabs/puppet/bin/r10k',
 }
 
+# Run r10k to grab the modules specified in the Puppetfile
 exec { 'r10k':
   command => '/opt/puppetlabs/puppet/bin/r10k puppetfile install',
   cwd     => '/vagrant/puppet/bootstrap',
