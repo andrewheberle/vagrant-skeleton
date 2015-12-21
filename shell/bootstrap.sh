7da3f368eaaa6afa45e50e22e072896dc264d0d6
Andrew Heberle <andrew.heberle@gmail.com>
2015-12-21 15:45:22 +0800

    Working logic for installing Pppet modules via r10k

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -28,13 +28,48 @@ declare PUPPETLABS_PKG
 # Puppet locations
 declare PUPPET_BASEPATH=${VAGRANT}/puppet
 declare PUPPET_ENVIRONMENT=bootstrap
+declare PUPPETFILE=${PUPPET_BASEPATH}/${PUPPET_ENVIRONMENT}/Puppetfile
 declare PUPPETLABS_OPT_DIR=/opt/puppetlabs
 declare PUPPET_GEM=${PUPPETLABS_OPT_DIR}/puppet/bin/gem
+declare PUPPET_R10K=${PUPPETLABS_OPT_DIR}/puppet/bin/r10k
 
 function cleanup() {
 	[ -f ${PUPPETLABS_PKG} ] && rm -f ${PUPPETLABS_PKG}
 }
 trap cleanup EXIT
+
+function install_r10k() {
+	if [ -f ${PUPPET_R10K} ]; then
+		echo "r10k already installed."
+		return 0
+	else
+		if [ -f "${PUPPET_GEM}" ]; then
+			echo "Installing r10k..."
+			${PUPPET_GEM} install r10k
+			return $?
+		else
+			echo "${PUPPET_GEM} was not found.  Cannot install r10k."
+			return 1
+		fi
+	fi
+}
+
+function run_r10k() {
+	if [ -f ${PUPPET_R10K} ]; then
+		echo "Installing Puppet modules via r10k..."
+		if [ -d $1 ]; then
+			pushd $1
+		else
+			pushd .
+		fi
+		${PUPPET_R10K} puppetfile install
+		popd
+		return $?
+	else
+		echo "${PUPPET_R10K} was not found."
+		return 1
+	fi
+}
 
 # Does lsb_release exist?
 $(which lsb_release > /dev/null 2>&1)
@@ -46,8 +81,7 @@ if [ "${FOUND_LSB_RELEASE}" -ne '0' ]; then
 	FOUND_LSB_RELEASE=$?
 fi
 
-if [ "${FOUND_LSB_RELEASE}" -eq '0' ]
-then
+if [ "${FOUND_LSB_RELEASE}" -eq '0' ]; then
 	DISTRO=$(lsb_release -s -i)
 else
 	DISTRO="ERROR"
@@ -80,8 +114,7 @@ esac
 echo "Downloading ${PUPPETLABS_PKG}..."
 ${HTTPGET} ${HTTPGET_OPTS} ${PUPPETLABS_PKG_URL}/${PUPPETLABS_PKG}
 RESULT=$?
-if [ $RESULT -eq 0 ]
-then
+if [ $RESULT -eq 0 ]; then
 	echo "Installing ${PUPPETLABS_PKG}..."
 	eval "${PKG_INSTALL_CMD}"
 fi
@@ -89,8 +122,7 @@ fi
 # Install basic packages
 # Limit to stuff required to bootstrap puppet-agent, everything else can be installed via Puppet
 PKG_LISTS="common ${DISTRO}"
-for LIST in ${PKG_LISTS}
-do
+for LIST in ${PKG_LISTS}; do
 	echo "Processing packages from ${LIST}..."
 	if [ -f "${BASEPATH}/packages.${LIST}" ]
 	then
@@ -103,18 +135,9 @@ do
 done
 
 # If Puppetfile exists, install r10k and install modules
-if [ -f "${PUPPET_BASEPATH}/${PUPPET_ENVIRONMENT}/Puppetfile" ]
-then
-	if [ -f "${PUPPET_GEM}" ]
-	then
-		${PUPPET_GEM} install r10k
-		if [ -f ${PUPPET_R10K} ]
-		then
-			pushd ${PUPPET_BASEPATH}/${PUPPET_ENVIRONMENT}
-			${PUPPET_R10K} puppetfile install
-			popd
-		fi
-	fi
+if [ -f "${PUPPETFILE}" ]; then
+	echo "Puppetfile found..."
+	install_r10k && run_r10k $(dirname ${PUPPETFILE})
 fi
 
 exit 0
