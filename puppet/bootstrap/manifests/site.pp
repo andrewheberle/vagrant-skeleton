855fe5b1cd2155dadc533f2304474926032e3055
Andrew Heberle <andrew.heberle@gmail.com>
2015-12-22 11:49:49 +0800

    Added node default to site.pp

--- puppet/bootstrap/manifests/site.pp
+++ puppet/bootstrap/manifests/site.pp
@@ -1,12 +1,3 @@
-# Install r10k
-exec { 'r10k-install':
-  command => '/opt/puppetlabs/puppet/bin/gem install r10k',
-  creates => '/opt/puppetlabs/puppet/bin/r10k',
-}
-
-# Run r10k to grab the modules specified in the Puppetfile
-exec { 'r10k':
-  command => '/opt/puppetlabs/puppet/bin/r10k puppetfile install',
-  cwd     => '/vagrant/puppet/bootstrap',
-  require => Exec['r10k-install'],
+node default {
+  # Add bootstrap code here
 }
